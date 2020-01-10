aa0fa0d6bc79d40ecab11840b3622c7d2316daf5
hasanNurubuloev <hasan.nurubuloev59@gmail.com>
2020-01-10 18:59:45 +0600

    /

--- app/src/main/java/com/example/weatherapp/ui/onboard/OnBoardActivity.java
+++ app/src/main/java/com/example/weatherapp/ui/onboard/OnBoardActivity.java
@@ -5,21 +5,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.weatherapp.R;
 import com.example.weatherapp.ui.base.BaseActivity;
+import com.example.weatherapp.ui.main.MainActivity;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.Objects;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class OnBoardActivity extends BaseActivity {
-
+    @BindView(R.id.viewpager)
+    ViewPager pager;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
-    @Override    
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+
+    Button next;
+
+    private int currentPage;
+
+    @Override
     protected int getViewLayout() {
         return R.layout.activity_on_board;
     }
@@ -29,6 +45,46 @@ public class OnBoardActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+
+        String texts[] = new String[]{"In the application", "in the application you can find out information about the weather",
+                "for attention"};
+        int[] pictures = new int[]{R.drawable.welcome, R.drawable.weather, R.drawable.thakn_you};
+
+        PagerAdapter adapter = new OnBoardAdapter(OnBoardActivity.this, texts, pictures);
+        pager.setAdapter(adapter);
+        pager.setCurrentItem(0);
+        tabLayout.setupWithViewPager(pager, true);
+
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+//                if (position )
+//                next.setVisibility();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+    }
+
+
+
+    public void onClick(View v) {
+        if (pager.getCurrentItem() != 2) {
+            pager.setCurrentItem(currentPage + 1);
+        } else {
+            startActivity(new Intent(OnBoardActivity.this, MainActivity.class));
+        }
     }
 
     @Override
@@ -38,7 +94,18 @@ public class OnBoardActivity extends BaseActivity {
         return true;
     }
 
-    public static void start(Context context){
+    public static void start(Context context) {
         context.startActivity(new Intent(context, OnBoardActivity.class));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.skip:
+                MainActivity.start(this);
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/weatherapp/ui/onboard/OnBoardAdapter.java
+++ app/src/main/java/com/example/weatherapp/ui/onboard/OnBoardAdapter.java
@@ -1,18 +1,64 @@
 package com.example.weatherapp.ui.onboard;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.arch.core.executor.TaskExecutor;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewpager.widget.PagerAdapter;
 
+import com.example.weatherapp.R;
+
+import butterknife.BindView;
+
 public class OnBoardAdapter extends PagerAdapter {
+    private Context context;
+    private String[] texts;
+    private int[] pictures;
+
+    public  OnBoardAdapter (Context context , String[] texts, int[] pictures ){
+        this.context = context;
+        this.texts = texts;
+        this.pictures = pictures;
+    }
     @Override
     public int getCount() {
-        return 0;
+        return texts.length;
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return false;
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        TextView boardText;
+        ImageView boardImage;
+
+        LayoutInflater inflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.item_onboard , container , false);
+
+        boardText = itemView.findViewById(R.id.tvTitle);
+        boardText.setText(texts[position]);
+
+        boardImage = itemView.findViewById(R.id.imgOnBoard);
+        boardImage.setImageResource(pictures[position]);
+
+        container.addView(itemView);
+        return itemView;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout) object);
     }
 }
